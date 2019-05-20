e4b050b9e2fce5dd99132d0f6fb373bb872d9b24
David Erick Avalos Guerra <david avalos@tiempodevelopment.com>
2019-05-20 16:39:36 -0500

    Addding test samples

--- test/protractor_test.js
+++ test/protractor_test.js
@@ -16,16 +16,59 @@ function perform_operation(a, b, operation_name){
 describe("Calculator sample", function(){
 
     beforeAll(function(){
+        results = ['9','2','0','18','3'];
+        iteration = 0;
         navigate('http://www.way2automation.com/angularjs-protractor/calc/');
     });
 
-    it("first test", function(){
+    it("Addition", function(){
         
-        perform_operation(2, 3, "SUBTRACTION");
-        element(by.css('.ng-binding')).getText().then(function(text){
-            console.log(text);
+        perform_operation(6, 3, "ADDITION");
+        element(by.css('.ng-binding')).getText().then(function(number){
+            expect(number).toBe(results[0]);
+        });   
+    });
+
+    it("Division", function(){
+        
+        perform_operation(6, 3, "DIVISION");
+        element(by.css('.ng-binding')).getText().then(function(number){
+            expect(number).toBe(results[1]);
+        });   
+    });
+
+    it("Modulo", function(){
+        
+        perform_operation(6, 3, "MODULO");
+        element(by.css('.ng-binding')).getText().then(function(number){
+            expect(number).toBe(results[2]);
+        });   
+    });
+
+    it("Multiplication", function(){
+        
+        perform_operation(6, 3, "MULTIPLICATION");
+        element(by.css('.ng-binding')).getText().then(function(number){
+            expect(number).toBe(results[3]);
+        });   
+    });
+
+    it("Subtraction", function(){
+        
+        perform_operation(6, 3, "SUBTRACTION");
+        element(by.css('.ng-binding')).getText().then(function(number){
+            expect(number).toBe(results[4]);
+        });   
+    });
+
+    it("Table validation", function(){
+
+        results = results.reverse();
+        element.all(by.repeater("result in memory")).each(function(item){
+            item.element(by.css("td:nth-child(3)")).getText().then(function(text){
+                expect(text).toBe(results[iteration]);
+                iteration++;
+            });
         });
-        browser.sleep(3000);
-   
     });
 });
