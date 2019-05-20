ea00bb502b54e31254e49053fd2bf71e40795295
David Erick Avalos Guerra <david avalos@tiempodevelopment.com>
2019-05-20 12:28:50 -0500

    Adding protractor test sample

--- test/protractor_test.js
+++ test/protractor_test.js
@@ -1,5 +1,31 @@
-describe("My First Protractor Test", function(){
-    it("Let's TEST!", function(){
-        browser.get('https://www.youtube.com/channel/UCrvBWaw7Dwo1zibJqo3RpuQ');
+function navigate(url)
+{
+    browser.driver.manage().window().maximize();
+    browser.get(url);
+}
+
+function perform_operation(a, b, operation_name){
+    element(by.model('first')).sendKeys(a);
+    element(by.model('second')).sendKeys(b);
+
+    browser.actions().mouseMove(element(by.model('operator'))).click().perform();
+    browser.actions().mouseMove(element(by.xpath('//option[@value=\''+ operation_name.toUpperCase() +'\']')).click()).perform();
+    element(by.id('gobutton')).click();
+}
+
+describe("Calculator sample", function(){
+
+    beforeAll(function(){
+        navigate('http://www.way2automation.com/angularjs-protractor/calc/');
+    });
+
+    it("first test", function(){
+        
+        perform_operation(2, 3, "SUBTRACTION");
+        element(by.css('.ng-binding')).getText().then(function(text){
+            console.log(text);
+        });
+        browser.sleep(3000);
+   
     });
 });
